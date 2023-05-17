0d0dce56540c8ab00f8d5b4b142aa1db1ff25819
char5742 <f.shuuma@gmail.com>
2023-05-17 13:32:59 +0900

    faet: 学習部でマルチスレッドを使用するように

--- src/training.jl
+++ src/training.jl
@@ -18,11 +18,11 @@ function calc_expected_reward(
     current_tspin_array = [(node.tspin > 0) |> Float32;;]
     current_holdnext_array = reshape(holdnext, 6, 7, 1)
 
-    current_expect_reward = lock(brain.mainlock) do
+    current_expect_reward =
         predict(brain.main_model,
             (current_gameboard_array, current_minopos_array,
                 current_combo_array, current_btb_array, current_tspin_array, current_holdnext_array))[1]
-    end
+ 
     if node.game_state.game_over_flag
         return -0.5, abs(-0.5 - current_expect_reward) + ϵ
     end
@@ -34,11 +34,11 @@ function calc_expected_reward(
 
     max_node_minopos = generate_minopos(max_node.mino, max_node.position)
     max_node_holdnext = reshape(vcat([mino_to_array(mino) for mino in [node.game_state.hold_mino, node.game_state.mino_list[end-4:end]...]]...), 6, 7, 1)
-    p = lock(brain.targetlock) do
+    p = 
         predict(brain.target_model,
             (vector2array([node.game_state.current_game_board.binary .|> Float32]), vector2array([max_node_minopos .|> Float32]), [node.game_state.combo |> Float32;;], [node.game_state.back_to_back_flag |> Float32;;], [(max_node.tspin > 0) |> Float32;;], max_node_holdnext,
             ))[1]
-    end
+   
     temporal_difference = scaled_reward + p * discount_rate - current_expect_reward
     expected_reward = p * discount_rate + scaled_reward
     return expected_reward, abs(temporal_difference) + ϵ
@@ -60,12 +60,12 @@ function qlearn(learner::Learner, batch_size, id_and_exp::Vector{Tuple{Int,Exper
         # 経験の新しい価値
         new_temporal_difference_list = Vector{Tuple{Int,Float32}}(undef, batch_size)
 
-        for (i, (id, (;
+        Threads.@threads for (i, (id, (;
             current_state,
             selected_node,
             next_node_list,
             temporal_difference
-        ))) in enumerate(id_and_exp)
+        ))) in collect(enumerate(id_and_exp))
             prev_game_board_array[:, :, 1, i] = current_state.current_game_board.binary
             minopos_array[:, :, 1, i] = generate_minopos(selected_node.mino, selected_node.position)
             prev_combo_array[i] = current_state.combo
@@ -85,8 +85,7 @@ function qlearn(learner::Learner, batch_size, id_and_exp::Vector{Tuple{Int,Exper
         end
         learner.taget_update_count += 1
         fit!(learner, (prev_game_board_array, minopos_array, prev_combo_array, prev_back_to_back_array, prev_tspin_array, prev_holdnext_array), expected_reward_array), sum(expected_reward_array) / batch_size, sum(prev_tspin_array), new_temporal_difference_list
-    catch e
-        rethrow(e)
+    catch
         GC.gc(true)
         0.0, 0.0, 0.0, Vector{Tuple{Int,Float32}}()
     end

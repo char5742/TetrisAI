137498af6c44aaf10e941a357e872c5f507e47a5
char5742 <f.shuuma@gmail.com>
2023-06-24 20:45:07 +0900

    feat: trainmode, testmodeを分けるように

--- src/ai/lux/training.jl
+++ src/ai/lux/training.jl
@@ -1,21 +1,19 @@
 function fit!(learner::Learner, x, y)
     x = x |> gpu
     y = y |> gpu
-    trainingloss = lock(learner.brain.mainlock) do
-        (l, st), back = pullback(learner.brain.main_model.ps) do ps
-            ŷ, st = learner.brain.main_model.model(x, ps, learner.brain.main_model.st)
-            mean(abs2.(ŷ .- y)), st
-        end
-        gs = back((one(l), nothing))[1]
-        learner.brain.main_model.st = st
-        st_opt, ps = Optimisers.update(learner.optim, learner.brain.main_model.ps, gs)
-        learner.brain.main_model.ps = ps
-        learner.optim = st_opt
-        l
+    _st = Lux.trainmode(learner.brain.main_model.st)
+    (trainingloss, st), back = pullback(learner.brain.main_model.ps) do ps
+        ŷ, st = learner.brain.main_model.model(x, ps, _st)
+        mean(abs2.(ŷ .- y)), st
     end
+    gs = back((one(trainingloss), nothing))[1]
+    learner.brain.main_model.st = Lux.testmode(st)
+    st_opt, ps = Optimisers.update(learner.optim, learner.brain.main_model.ps, gs)
+    learner.brain.main_model.ps = ps
+    learner.optim = st_opt
     x = nothing
     y = nothing
     trainingloss
 end
 
-create_optim(learning_rate, ps) = Optimisers.setup(Optimisers.AdaBelief(learning_rate), ps)
+create_optim(learning_rate, ps) = Optimisers.setup(Optimisers.AdamW(learning_rate), ps)

a9a2326842b9e5aa885c3961780cfcc95f079b94
char5742 <f.shuuma@gmail.com>
2023-10-08 21:55:45 +0900

    chore: 不要なファイルを削除

--- src/ai/lux/model.jl
+++
@@ -1,5 +0,0 @@
-mutable struct Model
-    model
-    ps
-    st
-end
--- src/component/experience.jl
+++
@@ -1,9 +0,0 @@
-# 一ステップの経験
-# 現在と次の盤面の情報が入る
-mutable struct Experience
-    current_state::GameState
-    selected_node::Node
-    next_node_list::Vector{Node}
-    temporal_difference::Float64
-end
-
